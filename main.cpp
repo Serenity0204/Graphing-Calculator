3fea7c9c38bc3f49b9e9ac84cc080cbd8e7817e8
Yu-Heng Lin <yuhenglin0204@gmail.com>
2022-11-18 16:46:14 -0800

    draw sin

--- main.cpp
+++ main.cpp
@@ -15,21 +15,25 @@ int main()
     // sf::Sprite sprite;
     // sprite.setTexture(texture, true);
 
-    sf::Font font;
-    font.loadFromFile("../assets/arial.ttf");
-    sf::Text text;
-    // select the font
-    text.setFont(font); // font is a sf::Font
-    // set the string to display
-    text.setString("Hello world");
-    // set the character size
-    text.setCharacterSize(24); // in pixels, not points!
-    // set the color
-    text.setFillColor(sf::Color::Red);
-    // set the text style
-    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
-
-
+    // sf::Font font;
+    // font.loadFromFile("../assets/arial.ttf");
+    // sf::Text text;
+    // // select the font
+    // text.setFont(font); // font is a sf::Font
+    // // set the string to display
+    // text.setString("Hello world");
+    // // set the character size
+    // text.setCharacterSize(24); // in pixels, not points!
+    // // set the color
+    // text.setFillColor(sf::Color::Red);
+    // // set the text style
+    // text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    sf::VertexArray function(sf::LinesStrip, 100);
+    function.clear();
+    for(float x = -30; x < 30; x+=0.25)
+    {
+        function.append(sf::Vertex(sf::Vector2f(x*20.f + window.getSize().x/2, -1.f*sin(pow(x, 2))*20.f + window.getSize().y / 2)));
+    }
     while (window.isOpen())
     {
         sf::Event event;
@@ -40,8 +44,8 @@ int main()
         }
 
         window.clear();
-
-        window.draw(text);
+        window.draw(function);
+        //window.draw(text);
         //window.draw(sprite);
         //window.draw(shape);
         window.display();
