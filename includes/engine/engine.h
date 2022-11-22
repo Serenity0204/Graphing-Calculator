4c31917c5ed065e8c0fdfa8a2cbaaad2d9347ca9
Yu-Heng Lin <yuhenglin0204@gmail.com>
2022-11-21 22:06:21 -0800

    added

---
+++ includes/engine/engine.h
@@ -0,0 +1,20 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+#include <bits/stdc++.h>
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+
+class Engine 
+{
+private:
+
+public:
+    Engine();
+    ~Engine();
+};
+
+
+
+
+#endif
